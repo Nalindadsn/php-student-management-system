0f2c34c41160c82c18bd26d56cefa624bedc2a90
Nalindadsn <nalindadsn@gmail.com>
2024-04-09 12:29:52 +0530

    add

--- login.php
+++ login.php
@@ -35,7 +35,7 @@ echo "<script>alert('Invalid username or password');</script>";
         <link href="css/styles.css" rel="stylesheet" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
     </head>
-    <body class="bg-primary">
+    <body  style="background: url('img/bg1.jpg'); background-size: cover">
         <div id="layoutAuthentication">
             <div id="layoutAuthentication_content">
                 <main>
@@ -45,9 +45,8 @@ echo "<script>alert('Invalid username or password');</script>";
                                 <div class="card shadow-lg border-0 rounded-lg mt-5">
 
 <div class="card-header">
-<h2 align="center">Registration and Login System</h2>
-<hr />
-    <h3 class="text-center font-weight-light my-4">User Login</h3></div>
+
+    <h3 class="text-center font-weight-light my-4">Student Login</h3></div>
                                     <div class="card-body">
                                         
                                         <form method="post">
@@ -65,13 +64,12 @@ echo "<script>alert('Invalid username or password');</script>";
 
 
 <div class="d-flex align-items-center justify-content-between mt-4 mb-0">
-<a class="small" href="password-recovery.php">Forgot Password?</a>
-<button class="btn btn-primary" name="login" type="submit">Login</button>
+<button class="btn btn-primary w-full" name="login" type="submit">Login</button>
 </div>
 </form>
 </div>
                                     <div class="card-footer text-center py-3">
-                                        <div class="small"><a href="signup.php">Need an account? Sign up!</a></div>
+                                       
                                           <div class="small"><a href="index.php">Back to Home</a></div>
                                     </div>
                                 </div>
