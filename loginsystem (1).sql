9deec62f07135e5fae0d53f3941aeea95a630098
Nalindadsn <nalindadsn@gmail.com>
2024-04-09 17:26:52 +0530

    add

--- loginsystem (1).sql
+++ loginsystem (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2024 at 09:39 AM
+-- Generation Time: Apr 09, 2024 at 01:55 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -43,6 +43,44 @@ INSERT INTO `admin` (`id`, `username`, `password`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `intake`
+--
+
+CREATE TABLE `intake` (
+  `id` int(11) NOT NULL,
+  `intake` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `intake`
+--
+
+INSERT INTO `intake` (`id`, `intake`) VALUES
+(2, '2014'),
+(3, '2013');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `notice`
+--
+
+CREATE TABLE `notice` (
+  `id` int(11) NOT NULL,
+  `notice` varchar(500) DEFAULT NULL,
+  `intake` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `notice`
+--
+
+INSERT INTO `notice` (`id`, `notice`, `intake`) VALUES
+(4, '  dfgdfg\r\n', 3);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -65,12 +103,12 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `fullName`, `nameWithInitials`, `indexNo`, `intake`, `fname`, `lname`, `email`, `password`, `contactno`, `posting_date`) VALUES
-(1, 'Suprabha jhjkhjj jhsd jkfhsjkdh kjhd jksf hskdjf h djshfkjdsfh ', 'S p', '', '2014', 'Rahul', 'Kumar', 'rahulj23@gmail.com', 'Test@123', '1234563210', '2024-02-18 06:15:52'),
-(2, NULL, NULL, '', NULL, 'John', 'Doe', 'johndoe12@gamil.com', 'Test@12345', '4563210236', '2024-02-19 06:35:03'),
-(3, NULL, NULL, '', NULL, 'fi', 'la', 'test@gmail.com', '941151663@Bandara', '0704238939', '2024-04-08 08:11:58'),
+(2, 'test1', 'S p', '', '2', 'John', 'Doe', 'johndoe12@gamil.com', 'Test@12345', '4563210236', '2024-02-19 06:35:03'),
+(3, '11test', 'S p', '', '2', 'fi', 'la', 'test@gmail.com', '941151663@Bandara', '0704238939', '2024-04-08 08:11:58'),
 (4, NULL, NULL, '', NULL, 'fa', 'la', 'nalindadsn@gmail.com', '941151663@Bandara', '0704238939', '2024-04-08 08:34:31'),
 (5, NULL, NULL, '', NULL, 'fa', 'la', 'suprabha@gmail.com', 'Dsn@8798761', '0704238939', '2024-04-08 08:41:16'),
-(6, '1Suprabha jhjkhjj jhsd jkfhsjkdh kjhd jksf hskdjf h djshfkjdsfh ', '1S p', '1666', '12014', '1fa', '1la', 'nalindadsn123@gmail.com', '941151663Bandara', '0704238939', '2024-04-09 07:33:02');
+(6, '1Suprabha jhjkhjj jhsd jkfhsjkdh kjhd jksf hskdjf h djshfkjdsfh ', '1S p', '1666', '12014', '1fa', '1la', 'nalindadsn123@gmail.com', '941151663Bandara', '0704238939', '2024-04-09 07:33:02'),
+(7, 'Suprabha jhjkhjj jhsd jkfhsjkdh kjhd jksf hskdjf h djshfkjdsfh ', 'S p', '123', '3', 'fi', 'la', 'testass@gmail.com', '941151663Bandara', '0704238939', '2024-04-09 11:23:13');
 
 --
 -- Indexes for dumped tables
@@ -80,6 +118,18 @@ INSERT INTO `users` (`id`, `fullName`, `nameWithInitials`, `indexNo`, `intake`, 
 -- Indexes for table `admin`
 --
 ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `intake`
+--
+ALTER TABLE `intake`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `notice`
+--
+ALTER TABLE `notice`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -99,10 +149,22 @@ ALTER TABLE `admin`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `intake`
+--
+ALTER TABLE `intake`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `notice`
+--
+ALTER TABLE `notice`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
